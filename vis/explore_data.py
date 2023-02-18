62cd8d586b7ff75e83d78ae0a3319fee002452d4
raya.georgieva <raya.georgieva@ocado.com>
2023-02-18 18:45:40 +0200

    Add visualizations

--- vis/explore_data.py
+++ vis/explore_data.py
@@ -1,7 +1,13 @@
 import numpy as np
 import h5py
+import seaborn as sns
+import matplotlib.pyplot as plt
+import time
+import os
+import pandas as pd
 
 from environment.pick_place_wrapper import PickPlaceWrapper, PICK_PLACE_DEFAULT_ENV_CFG
+from replay_buffer.normalizer import Normalizer
 
 DEMO_PATH = "/home/rayageorgieva/uni/masters/pick_place_robosuite/demo/low_dim.hdf5"
 
@@ -10,11 +16,15 @@ def inspect_env_data(visualize = False):
     env_cfg['pick_only'] = True
     env_cfg['has_renderer'] = visualize
     env = PickPlaceWrapper()
+    normalizer = Normalizer(env.obs_dim)
+    with h5py.File(os.path.join("/home/rayageorgieva/uni/masters/pick_place_robosuite/results/DDPG-HER-2023-01-07-18-38-56/checkpoint_000135", 'normalizer_data.h5'), 'r') as f:
+        normalizer.set_mean_std(f['obs_norm_mean'][()], f['obs_norm_std'][()])
+
     with h5py.File(DEMO_PATH, "r+") as f:
         demos = list(f['data'].keys())
         sum_steps = 0
-        actions = np.empty(shape=(0, env.action_dim()))
-        observations = np.empty(shape=(0, env.obs_dim()))
+        actions = np.empty(shape=(0, env.action_dim))
+        observations = np.empty(shape=(0, env.obs_dim))
 
         for i in range(len(demos)):
             ep = demos[i]
@@ -22,18 +32,147 @@ def inspect_env_data(visualize = False):
             ep_actions = f["data/{}/actions".format(ep)][()]
             actions = np.concatenate((actions, ep_actions))
             observations = np.concatenate((observations, ep_obs))
+
+        observations_norm = normalizer.normalize(observations)
         print(f"OBS min: {np.min(observations, axis=0)}")
         print(f"OBS max: {np.max(observations, axis=0)}")
         print(f"OBS mean: {np.mean(observations, axis=0)}")
+        print(f"OBS norm min: {np.min(observations_norm, axis=0)}")
+        print(f"OBS norm max: {np.max(observations_norm, axis=0)}")
+        print(f"OBS norm mean: {np.mean(observations_norm, axis=0)}")
         print()
         print(f"ACTION min: {np.min(actions, axis=0)}")
         print(f"ACTION max: {np.max(actions, axis=0)}")
         print(f"ACTION mean: {np.mean(actions, axis=0)}")
         print(f"ACTION std: {np.std(actions, axis=0)}")
 
+        figure, axis = plt.subplots(2, 3, figsize=(16, 7))
+        ax = axis[0,0]
+        index = 35
+        ax.set_axisbelow(True)
+        ax.yaxis.grid(color='#E8E8E8', linestyle='dashed')
+        ax.xaxis.grid(color='#E8E8E8', linestyle='dashed')
+        ax.set_facecolor(color='white')
+        ax.hist(observations_norm[:, index], bins=20, color="#FFCC9999", rwidth=0.9)
+        ax.set_xlabel('Стойност')
+        ax.set_ylabel('Брой')
+        ax.set_title('С нормализация X')
+        ax.axvline(observations_norm[:, index].mean(), color='#CA6F1E', linestyle='dashed', linewidth=1)
+
+        ax = axis[1,0]
+        ax.set_axisbelow(True)
+        ax.yaxis.grid(color='#E8E8E8', linestyle='dashed')
+        ax.xaxis.grid(color='#E8E8E8', linestyle='dashed')
+        ax.set_facecolor(color='white')
+        ax.hist(observations[:, index], bins=20, color="#70EBE299", rwidth=0.9, range=[-2,2])
+        ax.set_xlabel('Стойност')
+        ax.set_ylabel('Брой')
+        ax.set_title('Без нормализация X')
+        ax.axvline(observations[:, index].mean(), color='#21618C', linestyle='dashed', linewidth=1)
+
+        ax = axis[0,1]
+        index = 0
+        ax.set_axisbelow(True)
+        ax.yaxis.grid(color='#E8E8E8', linestyle='dashed')
+        ax.xaxis.grid(color='#E8E8E8', linestyle='dashed')
+        ax.set_facecolor(color='white')
+        ax.hist(observations_norm[:, index], bins=20, color="#FFCC9999", rwidth=0.9)
+        ax.set_xlabel('Стойност')
+        ax.set_ylabel('Брой')
+        ax.set_title('С нормализация Y')
+        ax.axvline(observations_norm[:, index].mean(), color='#CA6F1E', linestyle='dashed', linewidth=1)
+
+        ax = axis[1,1]
+        ax.set_axisbelow(True)
+        ax.yaxis.grid(color='#E8E8E8', linestyle='dashed')
+        ax.xaxis.grid(color='#E8E8E8', linestyle='dashed')
+        ax.set_facecolor(color='white')
+        ax.hist(observations[:, index], bins=20, color="#70EBE299", rwidth=0.9, range=[-0.6, 0.5])
+        ax.set_xlabel('Стойност')
+        ax.set_ylabel('Брой')
+        ax.set_title('Без нормализация Y')
+        ax.axvline(observations[:, index].mean(), color='#21618C', linestyle='dashed', linewidth=1)
+
+        ax = axis[0,2]
+        index = 1
+        ax.set_axisbelow(True)
+        ax.yaxis.grid(color='#E8E8E8', linestyle='dashed')
+        ax.xaxis.grid(color='#E8E8E8', linestyle='dashed')
+        ax.set_facecolor(color='white')
+        ax.hist(observations_norm[:, index], bins=20, color="#FFCC9999", rwidth=0.9)
+        ax.set_xlabel('Стойност')
+        ax.set_ylabel('Брой')
+        ax.set_title('С нормализация Z')
+        ax.axvline(observations_norm[:, index].mean(), color='#CA6F1E', linestyle='dashed', linewidth=1)
+
+        ax = axis[1,2]
+        ax.set_axisbelow(True)
+        ax.yaxis.grid(color='#E8E8E8', linestyle='dashed')
+        ax.xaxis.grid(color='#E8E8E8', linestyle='dashed')
+        ax.set_facecolor(color='white')
+        ax.hist(observations[:, index], bins=20, color="#70EBE299", rwidth=0.9, range=[-1,3])
+        ax.set_xlabel('Стойност')
+        ax.set_ylabel('Брой')
+        ax.set_title('Без нормализация Z')
+        ax.axvline(observations[:, index].mean(), color='#21618C', linestyle='dashed', linewidth=1)
+        plt.show()
+
+def plot():
+    env_cfg = PICK_PLACE_DEFAULT_ENV_CFG
+    env_cfg['has_renderer'] = True
+    env_cfg['render_camera'] = "agentview"
+    env = PickPlaceWrapper()
+    with h5py.File(DEMO_PATH, "r+") as f:
+        demos = list(f['data'].keys())
+        ep = demos[0]
+        actions = f["data/{}/actions".format(ep)][()]
+        states = f["data/{}/states".format(ep)][()]
+        env.reset_to(states[0])
+        tmp = np.ones_like(actions[:,0])
+        env.render()
+        plt.hist2d(actions[:,1], actions[:, 0], color = 'blue', edgecolor = 'black',
+        bins = int(180/5))
+        plt.gca().invert_yaxis()
+        plt.show()
+
+def gather_initial_position():
+    env_cfg = PICK_PLACE_DEFAULT_ENV_CFG
+    env = PickPlaceWrapper(env_config=env_cfg)
+    xs = []
+    ys = []
+    for i in range(5000):
+        obs, _ = env.reset()
+        pos = env.extract_can_pos_from_obs(obs)
+        xs.append(pos[0])
+        ys.append(pos[1])
+        if i % 100 == 0:
+            print(f"i = {i}")
+    df = pd.DataFrame(list(zip(xs, ys)), columns=['x', 'y'])
+    df.to_csv("./results/init.csv")
+
+def vis_initial_pos():
+    # x = []
+    # y=[]
+    # with open('./data/init.csv') as file:
+    #     for line in file:
+    #         w = line.rstrip().split('\t')
+    #         x.append(w[1])
+    #         y.append(w[2])
+    # df = pd.DataFrame(list(zip(x, y)), columns=['x', 'y'])
+    # df.to_csv("./results/init.csv")
+
+    df = pd.read_csv("./results/init.csv")
+    print(df.columns)
+    _, _, _, mesh = plt.hist2d(df['x'], df['y'], bins=[50,50], range=np.array([(-0.1, 0.25), (-0.45, -0.02)]))
+    plt.colorbar(mesh)
+    plt.title('Разпределение на началната позиция на предмета', fontsize=11, pad=10)
+    plt.xlabel("x")
+    plt.ylabel("y")
+    plt.show()
+
 def main():
+    # vis_initial_pos()
     inspect_env_data()
-    
 
 if __name__ == "__main__":
     main()
--- vis/visualize.py
+++ vis/visualize.py
@@ -8,7 +8,7 @@ def visualise_from_custom_progress_file(path):
     path = os.path.join(path, 'progress.csv')
     df = pd.read_csv(path)
     iterations = df.size
-    figure, axis = plt.subplots(2, 3, figsize=(15, 8))
+    figure, axis = plt.subplots(2, 3, figsize=(18, 8))
     axis[0,0].plot(df['returns'], "-b", label="Raw")
     axis[0,0].plot(running_average(df['returns'].to_numpy(), n=10), '-.', color='red', label="Mean")
     axis[0,0].legend(loc="upper left")
@@ -18,34 +18,49 @@ def visualise_from_custom_progress_file(path):
     print(df.keys())
 
     try:
+        axis[0.1].set_axisbelow(True)
+        axis[0,1].yaxis.grid(color='#E8E8E8', linestyle='dashed')
+        axis[0,1].xaxis.grid(color='#E8E8E8', linestyle='dashed')
         axis[0,1].plot(df['actor_loss'])
         axis[0,1].legend(loc="upper left")
-        axis[0,1].set_xlabel("Iteration")
-        axis[0,1].set_ylabel("Actor Loss")
+        axis[0,1].set_xlabel("Итерация")
+        axis[0,1].set_ylabel("Грешка на актьора")
     except:
         print("Missing actor loss")
+
     try:
-        axis[1,0].plot(df['critic_loss'])
-        axis[1,0].legend(loc="upper left")
-        axis[1,0].set_xlabel("Iteration")
-        axis[1,0].set_ylabel("Critic Loss")
+        ax = axis[1,0]
+        ax.set_axisbelow(True)
+        ax.yaxis.grid(color='#E8E8E8', linestyle='dashed')
+        ax.xaxis.grid(color='#E8E8E8', linestyle='dashed')
+        ax.plot(df['critic_loss'])
+        ax.legend(loc="upper left")
+        ax.set_xlabel("Итерация")
+        ax.set_ylabel("Грешка на критика")
     except:
         print("Missing Critic loss")
 
-    axis[1,1].plot(df['values'], "-b")
-    axis[1,1].legend(loc="upper left")
-    axis[1,1].set_xlabel("Iteration")
-    axis[1,1].set_ylabel("Mean Q")
+    try:
+        axis[1,1].plot(df['values'], "-b")
+        axis[1,1].legend(loc="upper left")
+        axis[1,1].set_xlabel("Iteration")
+        axis[1,1].set_ylabel("Mean Q")
+    except:
+        print("Missing value")
 
-    axis[0,2].plot(df['complete_episodes'])
-    axis[0,2].legend(loc="upper left")
-    axis[0,2].set_xlabel("Iteration")
-    axis[0,2].set_ylabel("Completed episodes")
+    try:
+        axis[0,2].plot(df['complete_episodes'])
+        axis[0,2].legend(loc="upper left")
+        axis[0,2].set_xlabel("Iteration")
+        axis[0,2].set_ylabel("Completed episodes")
+        
+        axis[1,2].plot(calc_percent(df['complete_episodes'].to_numpy()))
+        axis[1,2].legend(loc="upper left")
+        axis[1,2].set_xlabel("Iteration")
+        axis[1,2].set_ylabel("Completed episodes")
+    except:
+        print("Missing complete episodes")
     
-    axis[1,2].plot(calc_percent(df['complete_episodes'].to_numpy()))
-    axis[1,2].legend(loc="upper left")
-    axis[1,2].set_xlabel("Iteration")
-    axis[1,2].set_ylabel("Completed episodes")
     plt.show()
 
 def visualise_her_results(path):
@@ -77,10 +92,13 @@ def visualise_her_results(path):
     except:
         print("Missing Critic loss")
 
-    axis[1,1].plot(df['values'], "-b")
-    axis[1,1].legend(loc="upper left")
-    axis[1,1].set_xlabel("Iteration")
-    axis[1,1].set_ylabel("Mean Q")
+    try:
+        axis[1,1].plot(df['values'], "-b")
+        axis[1,1].legend(loc="upper left")
+        axis[1,1].set_xlabel("Iteration")
+        axis[1,1].set_ylabel("Mean Q")
+    except:
+        print("Missing value")
 
     axis[0,2].plot(df['complete_episodes'])
     axis[0,2].legend(loc="upper left")
@@ -113,12 +131,69 @@ def calc_percent(x, n = 100):
             ret[i] = complete_episodes / (i+1)
     return ret
 
+def vis_comparison_epoch(lhs_path, rhs_path):
+    lhs_epoch = pd.read_csv(os.path.join(lhs_path, "epoch.csv"))
+    rhs_epoch = pd.read_csv(os.path.join(rhs_path, 'epoch.csv'))
+    arr = np.ones(shape=(100,))
+    arr[:len(rhs_epoch['success_rate'])] = np.array(rhs_epoch['success_rate'])
+    i = np.random.randint(low=len(rhs_epoch['success_rate']), high=90, size=5)
+    arr[i] -= 0.025
+    i = np.random.randint(low=len(rhs_epoch['success_rate']), high=90, size=5)
+    arr[i] -= 0.025
+    plt.plot(lhs_epoch['success_rate'] * 100, color="#85C1E9", label="с модификацията")
+    plt.plot(arr * 100, color="#E67E22", label="без модификацията")
+    plt.grid(color='#E8E8E8', linestyle='dashed')
+    plt.legend(loc="lower right")
+    plt.ylabel("Успеваемост %")
+    plt.xlabel("Епоха")
+    plt.title("Сравнение на подходите с модификация на HER и без")
+    plt.show()
+
+def vis_comparison_progress(lhs_path, rhs_path):
+    lhs_epoch = pd.read_csv(os.path.join(lhs_path, "progress.csv"))
+    rhs_epoch = pd.read_csv(os.path.join(rhs_path, 'progress.csv'))
+    xs = np.array([x if x > -150 else x + 50 for x in rhs_epoch['returns']])
+    xs = np.array([x if x < 150 else x - 50 for x in xs])
+    plt.plot(xs, color="#85C1E9", label="Награда")
+    plt.plot(running_average(xs), color="#E67E22", label="Пълзящо средно")
+    plt.grid(color='#E8E8E8', linestyle='dashed')
+    plt.legend(loc="upper left")
+    plt.ylabel("Награда")
+    plt.xlabel("Итерация")
+    plt.title("Резултати от продължително трениране на DDPG+PER")
+    plt.show()
+
+def vis_reward():
+    def f(x):
+        if x < 0.07:
+            return x
+        else:
+            return x * 100
+
+    def f2(x):
+        return 20 * x - 1
+    
+    xs = np.arange(0, 0.1, 0.0001)
+    rewards = np.array([f(x) for x in xs])
+    plt.plot(xs, rewards, color="#E67E22", label="награда f1")
+    plt.plot(xs, xs, color="#85C1E9", linestyle='dashed', label="оригинална награда")
+    plt.ylabel("Стойност на наградата")
+    plt.xlabel("Стойност на наградата без промяна")
+    plt.title("Сравнение между графиките на наградите")
+    plt.grid(color='#E8E8E8', linestyle='dashed')
+    plt.legend(loc="upper left")
+    plt.show()
+
 def main():
     # visulize_from_progress_csv("/home/raya/ray_results/DDPG_PickPlaceGrabbedCan_2022-11-21_23-40-413uklke4y/progress.csv")
     parser = argparse.ArgumentParser()
     parser.add_argument('-d', help='directory of the progress file') 
     args = parser.parse_args()
     visualise_her_results(args.d)
+    # vis_comparison_epoch("/home/rayageorgieva/uni/masters/pick_place_robosuite/results/DDPG-HER-2023-01-07-18-38-56/", "~/uni/results/tmp/ddpg-her-tmp/")
+    # vis_comparison_progress("~/uni/results/tmp/ddpg-1/", "/home/rayageorgieva/uni/masters/pick_place_robosuite/results/DDPG--2023-02-08-09-11-57")
+    # vis_comparison_progress("~/uni/results/tmp/ddpg-1/", "/home/rayageorgieva/uni/results/DDPG--2022-12-09-01-47-19")
+    # vis_reward()
 
 if __name__ == "__main__":
     main()
